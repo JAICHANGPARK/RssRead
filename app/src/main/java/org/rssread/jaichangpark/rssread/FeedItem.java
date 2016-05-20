061220c2afaaa0b426005e9aef2118e60de130f2
PARK JAICHANG <JAICHANGPARK@PARK-JAICHANGs-MacBook-Pro.local>
2016-05-20 14:10:14 +0900

    Hello everyone Rss Reader Test CardView, RecycleView

---
+++ app/src/main/java/org/rssread/jaichangpark/rssread/FeedItem.java
@@ -0,0 +1,52 @@
+package org.rssread.jaichangpark.rssread;
+
+/**
+ * Created by JAICHANGPARK on 5/20/16.
+ */
+public class FeedItem {
+    String title;
+    String link;
+    String description;
+    String pubDate;
+    String thumbnailUrl;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubData) {
+        this.pubDate = pubDate;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
